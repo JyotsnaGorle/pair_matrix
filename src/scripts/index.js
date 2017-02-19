f19a04b91900099c4403466123257083c8abd67b
JyotsnaGorle <jyotsnagorle@gmail.com>
2017-02-19 17:06:16 +0530

    web: Dont know what I did

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,17 +1,31 @@
-(function($){
-  $(".person").click(function () {
-    alert("clicked");
-    var jqxhr = $.post( "/update/", { name: "jyotsna"}, function() {
-      alert( "success" );
-    })
-      .done(function() {
-        alert( "second success" );
-      })
-      .fail(function() {
-        alert( "error" );
-      })
-      .always(function() {
-        alert( "finished" );
-      });
-  });
+(function ($) {
+
+  function handle_first_person_selection() {
+    var listOfPersons1 = $(".person");
+
+    // $.each(listOfPersons1, function (index, eachPerson) {
+    //   eachPerson.click(function () {
+    //     $("#selected-person1").find("p").val(eachPerson.id);
+    //   })
+    // });
+    listOfPersons1.click(function () {
+      $("#selected-person1").find("p").text(listOfPersons1[0].className);
+    });
+  }
+
+  // $(".person").click(function () {
+  //   var jqxhr = $.post( "/update/", { name: "jyotsna"}, function() {
+  //     alert( "success" );
+  //   })
+  //     .done(function() {
+  //       alert( "second success" );
+  //     })
+  //     .fail(function() {
+  //       alert( "error" );
+  //     })
+  //     .always(function() {
+  //       alert( "finished" );
+  //     });
+  // });
+  handle_first_person_selection();
 })(jQuery);
